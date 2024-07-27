13f3170479b3cda20be736384a05fb6df10f6121
MilenaRickliS <engs-milenasilverio@camporeal.edu.br>
2024-07-26 21:25:29 -0300

    cc

--- exem2.php
+++ exem2.php
@@ -1,4 +1,5 @@
 <?php
+
     echo 'Verifica se número é primo: ';
     echo '<br>';
     $numero = 23;
@@ -6,14 +7,21 @@
  
     $divisores = 0;
     echo '<br>';
+
     for($count=2; $count<$numero; $count++)
     if($numero % $count == 0){
         echo "Multiplo de $count<br />";
         $divisores++;
     }
     echo '<br>';
-    if($divisores)  echo "Não é, tem $divisores divisores além de 1 e ele mesmo";
-    else    echo "É primo!";
+
+    if($divisores){
+        echo "Não é, tem $divisores divisores além de 1 e ele mesmo";
+    }
+    else if($numero < 0 || $numero == 1 || $numero == 0){
+        echo 'Não é primo (número negativo, igual a 1 ou igual a zero)';
+    }
+    else  echo "É primo!";
 
 
 ?>

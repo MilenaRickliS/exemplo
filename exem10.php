8d9e476ddf0d2d842a8866d0724a8c1acc1d5c41
MilenaRickliS <engs-milenasilverio@camporeal.edu.br>
2024-07-26 21:13:01 -0300

    cc

--- exem10.php
+++ exem10.php
@@ -1,4 +1,26 @@
 <?php 
 
+    function isPangram($text) {
+        $alphabet = range('a', 'z'); 
+        $text = strtolower($text); 
+
+        foreach ($alphabet as $letter) {
+            if (strpos($text, $letter) === false) {
+                return false; 
+            }
+        }
+
+        return true; 
+    }
+
+
+echo $text = "abcdefghijlmnopqrstuvxz";
+echo '<br>';
+
+if (isPangram($text)) {
+    echo "O texto é um pangrama!";
+} else {
+    echo "O texto não é um pangrama.";
+}
 
 ?>
